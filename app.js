14ea57af2b7c18a4fed6a734ac21d50eb058668e
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-04-18 21:15:05 +0530

    reading and writing into file

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
-const http = require('http');
+const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    console.log("Ujjwal");
-    res.setHeader('Content-type', 'text/html')
-    const url = req.url
-    if(url === '/home')
-    {
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Welcome Home!</h1></body>')
-        res.write('</html>')
-    }
-    if(url === '/about')
-    {
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Welcome to about us Page</h1></body>')
-        res.write('</html>')
-    }
-    if(url==="/node")
-    {
-        res.write('<html>')
-        res.write('<head><title>My First Page</title></head>')
-        res.write('<body><h1>Hello from Node.js!</h1></body>')
-        res.write('</html>')
-    }
-    
+  const url = req.url;
+  const method = req.method;
+  if (url === "/") {
+    fs.readFile("message.txt", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log("data from file >>>>>>", data);
+      res.write("<html>");
+      res.write("<head><title>Enter Message</title><head>");
+      res.write(`<body>${data}</body>`);
+      res.write(
+        '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+      );
+      res.write("</html>");
+
+      return res.end();
+    });
+  } else if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFile("message.txt", message, (err) => {
+        res.statusCode = 302;
+        res.setHeader("Location", "/");
+        return res.end();
+      });
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title><head>");
+    res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+    res.write("</html>");
     res.end();
+  }
 });
 
 server.listen(5000);
