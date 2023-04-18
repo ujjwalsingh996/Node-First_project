9e8fc2082b884b2c1e5bd8bd5a267478fd3ca756
ujjwalsingh996 <ujjwalsingh996@gmail.com>
2023-04-18 10:33:46 +0530

    routing

--- app.js
+++ app.js
@@ -2,6 +2,31 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("Ujjwal");
+    res.setHeader('Content-type', 'text/html')
+    const url = req.url
+    if(url === '/home')
+    {
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome Home!</h1></body>')
+        res.write('</html>')
+    }
+    if(url === '/about')
+    {
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome to about us Page</h1></body>')
+        res.write('</html>')
+    }
+    if(url==="/node")
+    {
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Hello from Node.js!</h1></body>')
+        res.write('</html>')
+    }
+    
+    res.end();
 });
 
-server.listen(4000);
+server.listen(5000);
